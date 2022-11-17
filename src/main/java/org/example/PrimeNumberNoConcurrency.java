1d24a4756f5cea0e556b119de138b1f423e2a1c3
Nitesh Ranjan <niteshr@thoughtworks.com>
2022-11-17 12:02:22 +0530

    Implement thread for prime calculation.

--- src/main/java/org/example/PrimeNumberNoConcurrency.java
+++ src/main/java/org/example/PrimeNumberNoConcurrency.java
@@ -4,15 +4,24 @@ import java.util.Scanner;
 
 public class PrimeNumberNoConcurrency {
     public void getPrimes() {
-        int number;
 
         while(true) {
             Scanner sc = new Scanner(System.in);
             System.out.println("\n Enter n to know the nth prime number: ");
             int n = sc.nextInt();
             if(n == 0) break;
-            number = PrimeNumberUtil.calculatePrime(n);
-            System.out.println("\n Value of " + n + "th prime: " + number);
+
+            Runnable r = new Runnable() {
+                @Override
+                public void run() {
+                    int number = PrimeNumberUtil.calculatePrime(n);
+                    System.out.println("\n Result: \n Value of " + n + "th prime: " + number);
+                }
+            };
+
+            Thread t = new Thread(r);
+            t.start();
+
         }
     }
 
